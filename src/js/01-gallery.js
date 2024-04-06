bcbb04ff37b2360f37bae86c97e25c179ca2d1e2
zetdou <zyziupoz@gmail.com>
2024-04-06 17:38:23 +0200

    Task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import simpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Change code below this line
+const photoGallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+    const galleryHTML = 
+    `<li>
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+        </a>
+    </li>`
+
+    photoGallery.insertAdjacentHTML('beforeend', galleryHTML);
+});
+
+const lightbox = new simpleLightbox('.gallery a');
+
+
