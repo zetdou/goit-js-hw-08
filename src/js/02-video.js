b1b4cfeb479676cbc1aa4763ec0f3c1ec3307706
zetdou <zyziupoz@gmail.com>
2024-04-06 18:08:48 +0200

    task-02
    Without lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+if (currentTime) {
+    player.setCurrentTime(Number.parseFloat(currentTime));
+};
+
+player.on("timeupdate", (data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+});
